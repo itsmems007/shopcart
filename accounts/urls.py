6255b264175e60db4133d286d4860b12ec17ce0b
itsmems007 <saxena.mohit59@gmail.com>
2020-08-15 19:19:14 +0530

    Initial Commit

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from .import views
+
+urlpatterns = [
+    path("signup",views.signup, name = "signup"),
+    path("login",views.handlelogin, name = "login"),
+    path("logout",views.handlelogout, name = "logout"),
+
+ ]
